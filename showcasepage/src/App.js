ccfe0f1db71fa46fed236d053588ffced877785a
xShadex <bartek.kwiczala@gmail.com>
2018-09-14 14:17:43 +0200

    added loading screen

--- showcasepage/src/App.js
+++ showcasepage/src/App.js
@@ -13,6 +13,25 @@ import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, faCheckSquare, faCoffee);
 
 class App extends Component {
+  // fake authentication Promise
+  authenticate() {
+    return new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
+  componentDidMount() {
+    this.authenticate().then(() => {
+      const ele = document.getElementById("ipl-progress-indicator");
+      if (ele) {
+        // fade out
+        ele.classList.add("available");
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = "";
+        }, 2000);
+      }
+    });
+  }
+
   render() {
     return (
       <Router>
