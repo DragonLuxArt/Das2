cb752b4e8038fc8d5170e647e9cad90f8ec40888
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2018-10-06 01:41:25 +0200

    rozklad jazdy

--- showcasepage/src/components/Pages/Contact.js
+++ showcasepage/src/components/Pages/Contact.js
@@ -5,6 +5,27 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
+      <CSSTransitionGroup
+      transitionName="example"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <h1>
+      DAS :: Rozkład jazdy naszych linii<br/>
+      </h1>
+      <p>
+      Prosze wybrać trasę:<br/>
+      Skoczów - Dębowiec centrum - Cieszyn<br/>
+      Cieszyn - Dębowiec Centrum - Skoczów<br/>
+      Cieszyn - Koniaków<br/>
+      Koniaków - Cieszyn<br/>
+      Kisielów - Dębowiec Centrum<br/>
+      Dębowiec Centrum - Kisielów<br/>
+      Cieszyn - Jaworzynka<br/>
+      Jaworzynka - Cieszyn<br/>
+      </p>
+      </CSSTransitionGroup>
       </div>
     );
   }
