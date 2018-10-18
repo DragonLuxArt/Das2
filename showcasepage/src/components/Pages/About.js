5a7f564adb4090bfb4ba5baa92a27aac97b1aa65
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2018-10-18 17:48:15 +0200

    added ONas page

--- showcasepage/src/components/Pages/About.js
+++ showcasepage/src/components/Pages/About.js
@@ -11,11 +11,21 @@ class About extends Component {
       transitionAppearTimeout={500}
       transitionEnter={false}
       transitionLeave={false}>
-      <h1>Firma DAS świadczy usługi w zakresie:</h1>
-       <p>regularnej komunikacji autobusowej,<br/>
-       wynajmu autokarów dla przewozów:<br/>
-       turystycznych i wycieczkowych,<br/>
-        krajowych oraz zagranicznych.</p>
+      <h1>Z nami do celu...</h1>
+      <div className="WraperLeft">
+      <p className="TextLeft">Firma DAS świadczy usługi w zakresie<br/>
+       regularnej komunikacji autobusowej,<br/>
+       wynajmu autokarów dla przewozów<br/>
+       turystycznych i wycieczkowych- krajowych oraz zagranicznych.</p>
+      </div>
+      <div className="WraperRight">
+      <p className="TextRight">Świadczymy usługi regularnej<br/>
+      komunikacji autobusowej na trasach:<br/>
+      Dębowiec Centrum - Cieszyn<br/>
+      Jaworzynka - Cieszyn<br/>
+      Koniaków - Cieszyn<br/>
+      Dębowiec Centrum - Kisielów Remiza</p>
+      </div>
       </CSSTransitionGroup>
       </div>
     );
